c50b0ee55a11202ee670cf04bcca5b0e0ffbdaa7
SaakshamKindra <saakshamkindra@gmail.com>
2026-08-08 21:10:54 +0530

    Time: 2 ms (98.28%), Space: 43.1 MB (73.6%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isValid(String s) {
+       Stack<Character> st=new Stack<>();
+       for(char c:s.toCharArray()){
+        if(c=='('||c=='['||c=='{') st.push(c);
+        else{
+            if(st.empty()) return false;
+            char x=st.pop();
+            if(c==')'&&x!='('||c==']'&&x!='['||c=='}'&&x!='{') return false;
+        }
+       } 
+       return st.empty();
+    }
+}
